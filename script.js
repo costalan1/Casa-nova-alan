adba0742ffd0fe7d68ff42bdab082cbba5bead38
costalan1 <alancosta294@gmail.com>
2025-04-30 18:16:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -58,21 +58,21 @@ document.getElementById("gift-form").addEventListener("submit", function (e) {
   giftDoc.update({ available: true, nome, email }).then(() => {
     alert(`Obrigado, ${name}! Presente confirmado.`);
 
-// Envia o e-mail para você
-emailjs.send("service_gmail", "template_lq3gppr", {
-  to_name: "Alan",
-  to_email: "alancosta294@gmail.com",
-  from_name: name,
-  from_email: email,
-  gift: giftId
-})
-.then((response) => {
-  console.log("Email enviado com sucesso!", response);
-})
-.catch((error) => {
-  console.error("Erro ao enviar o email:", error);
-  alert("Houve um erro ao enviar o email. Tente novamente.");
-});
+    // Envia o e-mail para você
+    emailjs.send("service_gmail", "template_lq3gppr", {
+      to_name: "Alan",
+      to_email: "alancosta294@gmail.com",
+      from_name: name,
+      from_email: email,
+      gift: giftId
+    })
+    .then((response) => {
+      console.log("Email enviado com sucesso!", response);
+    })
+    .catch((error) => {
+      console.error("Erro ao enviar o email:", error);
+      alert("Houve um erro ao enviar o email. Tente novamente.");
+    });
 
     document.getElementById("form-container").style.display = "none";
     carregarPresentes(); // Atualiza a lista removendo item selecionado
